86e2dc878b2159d3579c067cbb715aadf7e40abf
Orestis Meikopoulos <diktyas1988@gmail.com>
2024-01-16 19:15:13 +0200

    Some refactorings on IOBoundOperation example.

--- Day 2/AsyncAwait/IOBoundOperation/Program.cs
+++ Day 2/AsyncAwait/IOBoundOperation/Program.cs
@@ -1,60 +1,55 @@
 ﻿using System;
 using System.Net.Http;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace IOBoundOperation
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static async Task Main()
         {
-            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} starts execution of Main.");
+            Console.WriteLine($"Thread {Environment.CurrentManagedThreadId} starts execution of {nameof(Main)}.");
 
-            var resultTask = GetFirstCharactersCountAsync("http://www.dotnetfoundation.org", 10);
+            var resultTask = GetFirstCharactersAsync("http://www.dotnetfoundation.org", 10);
 
-            for (int i = 0; i < 10; i++)
+            for (var i = 0; i < 10; i++)
             {
-                Console.WriteLine($"Doing random work on thread {Thread.CurrentThread.ManagedThreadId} in Main.");
+                Console.WriteLine($"Doing random work on thread {Environment.CurrentManagedThreadId} in {nameof(Main)}.");
             }
 
             var result = await resultTask;
 
-            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} result in Main: {result}");
+            Console.WriteLine($"Thread {Environment.CurrentManagedThreadId} result in {nameof(Main)}: {result}");
 
             Console.ReadLine();
         }
 
-        private static async Task<string> GetFirstCharactersCountAsync(string url, int count)
+        private static async Task<string> GetFirstCharactersAsync(string url, int count)
         {
-            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} starts execution of GetFirstCharactersCountAsync.");
+            Console.WriteLine($"Thread {Environment.CurrentManagedThreadId} starts execution of {nameof(GetFirstCharactersAsync)}.");
 
             // Execution is synchronous here
-            var client = new HttpClient();
+            using var client = new HttpClient();
 
             var pageTask = client.GetStringAsync(url);
 
-            for (int i = 0; i < 10; i++)
+            // Try to change here the value from 10 -> 10000 to see how the await pageTask 
+            // below will run synchronously as the Task will have already been completed.
+            for (var i = 0; i < 10; i++)
             {
-                Console.WriteLine($"Doing random work on thread {Thread.CurrentThread.ManagedThreadId} in GetFirstCharactersCountAsync.");
+                Console.WriteLine($"Doing random work on thread {Environment.CurrentManagedThreadId} in {nameof(GetFirstCharactersAsync)}.");
             }
 
-            // Execution of GetFirstCharactersCountAsync() is yielded to the caller here
+            // Execution of GetFirstCharactersAsync() is yielded to the caller here
             // GetStringAsync returns a Task<string>, which is *awaited*
             var page = await pageTask;
 
-            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} resumes execution of GetFirstCharactersCountAsync when the client.GetStringAsync task completes.");
+            Console.WriteLine($"Thread {Environment.CurrentManagedThreadId} resumes execution of {nameof(GetFirstCharactersAsync)} when the client.GetStringAsync task completes.");
 
             // Execution resumes when the client.GetStringAsync task completes,
             // becoming synchronous again.
-            if (count > page.Length)
-            {
-                return page;
-            }
-            else
-            {
-                return page.Substring(0, count);
-            }
+            return count > page.Length ?
+                page : page[..count];
         }
     }
 }
