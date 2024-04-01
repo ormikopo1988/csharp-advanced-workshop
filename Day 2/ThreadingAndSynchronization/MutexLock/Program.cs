391fd72f077da8f3cde13da62a812353ea28ef82
Orestis Meikopoulos <diktyas1988@gmail.com>
2024-04-01 21:11:01 +0300

    MutexLock project updated.

--- Day 2/ThreadingAndSynchronization/MutexLock/Program.cs
+++ Day 2/ThreadingAndSynchronization/MutexLock/Program.cs
@@ -1,19 +1,21 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
+using System;
 
 namespace MutexLock
 {
-    class Program
+    public class Program
     {
-        static Mutex mutex = new Mutex(false, "CustomOsMutex");
+        static readonly Mutex mutex = new(false, "CustomOsMutex");
 
-        static void Main(string[] args)
+        public static void Main()
         {
             // 10 different threads will try to acquire the mutex
-            for (int i = 0; i < 10; i++)
+            for (var i = 0; i < 10; i++)
             {
-                Thread thread = new Thread(AcquireMutex);
-                thread.Name = $"Thread{i+1}";
+                var thread = new Thread(AcquireMutex)
+                {
+                    Name = $"Thread{i + 1}"
+                };
                 thread.Start();
             }
 
@@ -22,16 +24,21 @@ namespace MutexLock
 
         private static void AcquireMutex()
         {
-            // WaitOne blocks the current thread until the current instance receives a signal. It returns true when the Mutex is signaled to indicate that it is not owned
-            // Try to acquire the mutex. If it is not able to acquire for the specified TimeSpan (1 second in this case) then it will not try again.
-            // As a result of this code only 1 thread will be able to acquire the Mutex. All the others will step into this if statement and return.
+            // WaitOne blocks the current thread until the current instance receives a signal.
+            // It returns true when the Mutex is signaled to indicate that it is not owned.
+            // Try to acquire the mutex.
+            // If it is not able to acquire for the specified TimeSpan (1 second in this case) then it will not try again.
+            // As a result of this code only 1 thread will be able to acquire the Mutex.
+            // All the others will step into this if statement and return.
             if (!mutex.WaitOne(TimeSpan.FromSeconds(1), false))
             {
                 Console.WriteLine($"{Thread.CurrentThread.Name} competed but was not able to acquire the Mutex within the specified TimeSpan.");
                 return;
             }
 
-            // Try to acquire the mutex. Block until it will be able to. All threads will acquire the Mutex.
+            // Try to acquire the mutex.
+            // Block until it will be able to.
+            // All threads will acquire the Mutex.
             //mutex.WaitOne();
             Console.WriteLine($"Mutex acquired by {Thread.CurrentThread.Name}.");
             DoSomething();
@@ -41,8 +48,9 @@ namespace MutexLock
 
         private static void DoSomething()
         {
-            // Simulate a long running operation that needs to be protected and thread safe.
+            // Simulate a long running operation
+            // that needs to be protected and thread safe.
             Thread.Sleep(6000);
-        } 
+        }
     }
 }
