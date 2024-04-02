10a4f406c97e014ce8bb0d0fe436a91e2a1c6fcb
Orestis Meikopoulos <diktyas1988@gmail.com>
2024-04-02 14:51:34 +0300

    Fix on ReaderWriterLocks

--- Day 2/ThreadingAndSynchronization/ReaderWriterLocks/Program.cs
+++ Day 2/ThreadingAndSynchronization/ReaderWriterLocks/Program.cs
@@ -1,21 +1,22 @@
 ﻿using System.Collections.Generic;
 using System.Threading;
 using System;
+using ReaderWriterLocks;
 
 var threads = new List<Thread>
 {
-    new Thread(SafeReadWriter.ReadSharedResource),
-    new Thread(SafeReadWriter.ReadSharedResource),
-    new Thread(SafeReadWriter.ReadSharedResource),
-    new Thread(() =>
+    new(SafeReadWriter.ReadSharedResource),
+    new(SafeReadWriter.ReadSharedResource),
+    new(SafeReadWriter.ReadSharedResource),
+    new(() =>
     {
         SafeReadWriter.WriteToSharedResource(1000, 30);
     }),
-    new Thread(SafeReadWriter.ReadSharedResource),
-    new Thread(SafeReadWriter.ReadSharedResource),
-    new Thread(SafeReadWriter.ReadSharedResource),
-    new Thread(SafeReadWriter.ReadSharedResource),
-    new Thread(() =>
+    new(SafeReadWriter.ReadSharedResource),
+    new(SafeReadWriter.ReadSharedResource),
+    new(SafeReadWriter.ReadSharedResource),
+    new(SafeReadWriter.ReadSharedResource),
+    new(() =>
     {
         SafeReadWriter.WriteToSharedResource(1000, 50);
     })
